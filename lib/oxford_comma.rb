06606d8f96681e748588ca09f102569847f15efc
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-12 01:36:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length === 1
+        array.join
+    elsif array.length === 2
+        array.join(" and ")
+    else
+        array.insert(-2, "and #{array[array.length-1]}")
+        array.pop
+        array.join(", ")
+    end
 end
