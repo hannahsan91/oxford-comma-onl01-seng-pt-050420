eae94d0eff32b1e76f3dbe4b63d366d1ed380313
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-12 01:35:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,11 @@
 def oxford_comma(array)
-  ["kiwi", "durian", "starfruit"].join
-  "kiwi :-) durian :-) starfruit"
+    if array.length === 1
+        array.join
+    elsif array.length === 2
+        array.join(" and ")
+    else
+        array.insert(-2, "and #{array[array.length-1]}")
+        array.pop
+        array.join(", ")
+    end
 end
